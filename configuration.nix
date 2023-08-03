0928c9d3a96b3760a683582f78fb726dec870286
Michael Fliegner <12422482+michaelfliegner@users.noreply.github.com>
2023-08-03 19:48:53 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -21,6 +21,20 @@
     settings.PasswordAuthentication = true;
   };
 
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = [ "mydatabase" ];
+    enableTCPIP = true;
+    port = 5432;
+    authentication = pkgs.lib.mkOverride 10 ''
+      #type database  DBuser  auth-method
+      local all       all     trust
+      # ipv4
+      host  all      all     127.0.0.1/32   trust
+      # ipv6
+      host all       all     ::1/128        trust
+      '';
+  };
   # Get package names from https://search.nixos.org/packages
   # These system packages will be installed
   environment.systemPackages = with pkgs; [
